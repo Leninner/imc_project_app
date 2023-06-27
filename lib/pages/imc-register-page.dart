154ec88078eeeabd520b97af9ecde580886d057f
JairP <jairparedesal123@gmail.com>
2023-06-27 16:00:02 -0500

    add function to the screen and connect the api

--- lib/pages/imc-register-page.dart
+++ lib/pages/imc-register-page.dart
@@ -1,46 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:imc_project_app/widgets/button_widget.dart';
+import 'package:imc_project_app/widgets/custom_text_field.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+import '../constants/app_routes.dart';
 import '../main.dart';
 import '../widgets/custom_button.dart';
-import '../widgets/custom_text_field.dart';
 
-class ImcPage extends StatelessWidget {
-  const ImcPage({super.key});
+class ImcPage extends StatefulWidget {
+  const ImcPage({Key? key}) : super(key: key);
+
+  @override
+  _ImcPageState createState() => _ImcPageState();
+}
+
+class _ImcPageState extends State<ImcPage> {
+  double imc = 0.0;
+  String resultadoTexto = '';
+
+  calcularImc(weight, height){
+    setState(() {
+      imc = double.parse(weight) /
+          (((double.parse(height)) / 100) * ((double.parse(height)) / 100));
+      resultadoTexto = double.parse(imc.toStringAsFixed(2)).toString();
+    });
+  }
+
+  Future<void> _PostImcData(String weight, String height) async {
+    calcularImc(weight, height);
+    await supabase
+        .from('user_imc')
+        .insert({
+      'userId':supabase.auth.currentUser!.id,
+      'height': height,
+      'weight':weight,
+      'imc':imc,
+    });
+  }
+
+  limpiarDatos(){
+    _weightController.clear();
+    _heightController.clear();
+    setState(() {
+      resultadoTexto = '';
+    });
+  }
+
+  TextEditingController _weightController = TextEditingController();
+  TextEditingController _heightController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-
-
-
-    Future<void> _Getdata() async {
-      final data = await supabase
-          .from('user_imc')
-          .select('*');
-      print(data);
-    };
-
-    Future<void> _PostImcData(weight,height) async {
-
-      await supabase
-          .from('user_imc')
-          .insert({
-            'userId':supabase.auth.currentUser!.id,
-            'height': height,
-            'weight':weight,
-          });
-    };
-    TextEditingController _weightController = TextEditingController();
-    TextEditingController _heightController = TextEditingController();
-
-
-
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushReplacementNamed(Routes.home);
+        },
+        backgroundColor: Colors.purple[800],
+        child: const Icon(Icons.arrow_back),
+      ),
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Calculadora de IMC',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Roboto',
+              fontSize: 22.0,
+            ),
+          ),
+        ),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const SizedBox(height: 20,),
+
+          const SizedBox(height: 20),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -52,17 +88,19 @@ class ImcPage extends StatelessWidget {
                 ),
               ),
               const Expanded(
-                child: Text('CM Ejemplo:(175 cm)',
+                child: Text(
+                  'CM Ejemplo:(175 cm)',
                   style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
                     fontFamily: 'Roboto',
+                    fontSize:  16,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
             ],
           ),
-          const SizedBox(height: 10,),
+          const SizedBox(height: 10),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -74,43 +112,109 @@ class ImcPage extends StatelessWidget {
                 ),
               ),
               const Expanded(
-                child: Text('KG Ejemplo:(70,5 kg)',
+                child: Text(
+                  'KG Ejemplo:(70,5 kg)',
                   style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
                     fontFamily: 'Roboto',
+                    fontSize:  16,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
             ],
           ),
-          const SizedBox(height: 10,),
+          const SizedBox(height: 40),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(child: const SizedBox(width: 1,)),
               Expanded(
-                child: CustomTextField(
-                  keyboardType: TextInputType.number,
-                  hintText: 'IMC RESULTANTE',
+                child:Center(
+                  child: Text(
+                    'Su indice de masa corporal es: $resultadoTexto',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto',
+                    ),
+                  ),
                 ),
               ),
-              Expanded(child: const SizedBox(width: 1,)),
             ],
           ),
-          const SizedBox(height: 10,),
+          const SizedBox(height: 20),
           CustomButton(
-            onTap: (){
-              _PostImcData(_weightController.text,_heightController.text);
+            onTap: () async {
+              if(_weightController.text =='' || _heightController.text ==''){
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                        'Ingrese datos en los campos correspondientes'),
+                    duration: const Duration(seconds: 5),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              }else if(toInt(_weightController.text)! > 20 && toInt(_weightController.text)! < 300  &&
+                       toInt(_heightController.text)! > 20 && toInt(_heightController.text)! < 300 ){
+                calcularImc(_weightController.text, _heightController.text);
+              }else{
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                        'Ingrese datos entre 20 y 300'),
+                    duration: const Duration(seconds: 5),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+
+              }
             },
             buttonText: 'CALCULAR',
-            verticalSizeButton: 10,
-            horizontalMarginButton: 125,
-            buttonColor: Colors.deepPurple,
-            textColor: Colors.white,
             borderRadiusButton: 10,
+            buttonColor: Colors.purple[300],
+            textColor: Colors.white,
+            verticalSizeButton: 15,
+            horizontalMarginButton: 80,
+            fontSizeButton: 20,
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(height: 20),
+          CustomButton(
+            onTap: () async {
+              if(_weightController ==null || _heightController == null){
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                        'Ingrese datos en los campos correspondientes'),
+                    duration: const Duration(seconds: 5),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              }else if(toInt(_weightController.text)! > 20 && toInt(_weightController.text)! < 300  ||
+                  toInt(_heightController.text)! > 20 && toInt(_heightController.text)! < 300 ){
+                await _PostImcData(
+                    _weightController.text, _heightController.text);
+                limpiarDatos();
+              }else{
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                        'Error en el ingreso de datos, revise y vuelva a intentar'),
+                    duration: const Duration(seconds: 5),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+
+              }
+            },
+            buttonText: 'GUARDAR DATOS',
+            borderRadiusButton: 10,
+            buttonColor: Colors.purple[800],
+            textColor: Colors.white,
+            verticalSizeButton: 15,
+            horizontalMarginButton: 80,
+            fontSizeButton: 20,
+          ),
+          const SizedBox(height: 20),
           Container(
             decoration: BoxDecoration(
               color: Colors.grey[200],
@@ -122,4 +226,10 @@ class ImcPage extends StatelessWidget {
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ImcPage(),
+  ));
 }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,53 +1,67 @@
-
-
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
-class CustomTextField extends StatelessWidget{
-  CustomTextField({Key? key,
-  required this.hintText,
-  this.keyboardType,
-  this.controller,
-
-    sizeButton,
+class CustomTextField extends StatelessWidget {
+  CustomTextField({
+    Key? key,
+    required this.hintText,
+    this.keyboardType,
+    this.controller,
+    this.onTap,
+    this.sizeButton,
   }) : super(key: key);
 
-  String? hintText;
-  double? sizeButton;
-  TextInputType? keyboardType;
-  TextEditingController? controller;
-
-
-
+  final String? hintText;
+  final double? sizeButton;
+  final TextInputType? keyboardType;
+  final TextEditingController? controller;
+  final VoidCallback? onTap;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-         Padding (
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: sizeButton ??5.00),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: sizeButton ?? 5.00),
           child: TextField(
             controller: controller,
             keyboardType: keyboardType ?? TextInputType.text,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$')),
+            ],
             decoration: InputDecoration(
               filled: true,
               fillColor: Color(0xFFF2F2F3),
-              border: const OutlineInputBorder(
+              border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10)),
                 borderSide: BorderSide.none,
               ),
               hintText: hintText,
-              hintStyle: const TextStyle(
+              hintStyle: TextStyle(
                 color: Color(0xFF2C2B47),
                 fontSize: 16,
-                fontFamily: 'FONTSPRING DEMO - Aftika Bold',
-              fontWeight: FontWeight.bold)
-
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.bold,
+              ),
+              errorText: _getErrorText(),
             ),
           ),
         ),
       ],
     );
   }
+
+  String? _getErrorText() {
+    if (controller?.text.isEmpty ?? true) {
+      return null;
+    }
+
+    final isValidNumber = RegExp(r'^\d*\.?\d*$').hasMatch(controller!.text);
+    if (!isValidNumber) {
+      return 'Ingresa solo números';
+    }
+
+    return null;
   }
+}
